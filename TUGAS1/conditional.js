c414068febc768bd4f1181f509f5b7d4b330ea54
mamaduye <154441636+mamaduye@users.noreply.github.com>
2024-03-28 12:31:33 +0700

    Add files via upload

---
+++ TUGAS1/conditional.js
@@ -0,0 +1,14 @@
+function checkNumber(tinggiBadan) {
+    // TODO: write your code here
+    if (tinggiBadan >= 170) {
+        console.log("Respect abangkuv....");
+    } else if (tinggiBadan >= 160) {
+        console.log("Lumayan....");
+    } else {
+        console.log("Tumbuh tuh keatas bukan kedalem");
+    }
+}
+
+checkNumber(150);
+checkNumber(165);
+checkNumber(179);
